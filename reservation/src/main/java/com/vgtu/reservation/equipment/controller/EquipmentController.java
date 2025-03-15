9c9943832f7b315502a92f94bce6f780095f1172
Artiom <artas.ple@gmail.com>
2025-03-15 18:43:56 +0200

    Changed Car/Equipment Controller annotations More equipment mock data Get equipment method

--- reservation/src/main/java/com/vgtu/reservation/equipment/controller/EquipmentController.java
+++ reservation/src/main/java/com/vgtu/reservation/equipment/controller/EquipmentController.java
@@ -1,4 +1,22 @@
 package com.vgtu.reservation.equipment.controller;
 
+import com.vgtu.reservation.equipment.entity.Equipment;
+import com.vgtu.reservation.equipment.service.EquipmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
 public class EquipmentController {
+
+    private final EquipmentService equipmentService;
+
+    @GetMapping("/equipment")
+    public List<Equipment> getEquipment() {
+        return equipmentService.getEquipment();
+    }
 }
