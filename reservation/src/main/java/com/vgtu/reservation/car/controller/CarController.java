d384f7739da6b1ef31a580d65d91a1347fbb64e9
Artiom <artas.ple@gmail.com>
2025-03-15 19:06:29 +0200

    Added description to GET equipment and car methods

--- reservation/src/main/java/com/vgtu/reservation/car/controller/CarController.java
+++ reservation/src/main/java/com/vgtu/reservation/car/controller/CarController.java
@@ -2,6 +2,7 @@ package com.vgtu.reservation.car.controller;
 
 import com.vgtu.reservation.car.entity.Car;
 import com.vgtu.reservation.car.service.CarService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class CarController {
 
     private final CarService carService;
 
+    @Operation(summary = "Get all cars", description = "Retrieves all cars from database")
     @GetMapping("/car")
     public List<Car> getCar() {
         return carService.getCar();
--- reservation/src/main/java/com/vgtu/reservation/equipment/controller/EquipmentController.java
+++ reservation/src/main/java/com/vgtu/reservation/equipment/controller/EquipmentController.java
@@ -2,6 +2,7 @@ package com.vgtu.reservation.equipment.controller;
 
 import com.vgtu.reservation.equipment.entity.Equipment;
 import com.vgtu.reservation.equipment.service.EquipmentService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class EquipmentController {
 
     private final EquipmentService equipmentService;
 
+    @Operation(summary = "Get all equipment", description = "Retrieves all equipment from database")
     @GetMapping("/equipment")
     public List<Equipment> getEquipment() {
         return equipmentService.getEquipment();
